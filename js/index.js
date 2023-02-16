017475cd8879a43ad0a160b78dd51036c47441d2
iamgo100 <gufkz.tkrbyf@yandex.ru>
2023-02-17 02:16:20 +0300

    fix8

--- js/index.js
+++ js/index.js
@@ -44,8 +44,7 @@ else {
     title.textContent = 'Отдел непонимания';
     document.querySelector('#task').innerHTML = `Мы не понимаем, куда вы пришли`;
 }
-let data = [];
-data = getData('db/data.json', src, data);
+let data = []; getData('db/data.json', src, data);
 console.log(data);
 
 let carts = '';
